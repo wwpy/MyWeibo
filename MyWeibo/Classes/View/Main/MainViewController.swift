52db574502e766cccf5b251ce6dc447b8d22f57d
wwyy <15268131701@163.com>
2020-10-26 12:39:17 +0800

    微博项目MVVM模式架构

---
+++ MyWeibo/Classes/View/Main/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  MyWeibo
+//
+//  Created by 王武 on 2020/10/26.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
---
+++ MyWeibo/SceneDelegate.swift
@@ -0,0 +1,61 @@
+//
+//  SceneDelegate.swift
+//  MyWeobo
+//
+//  Created by 王武 on 2020/10/26.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = MainViewController()
+        self.window = window
+        
+        window.makeKeyAndVisible()
+    }
+
+    // 场景没有连接时调用(等一会儿他会再连接)
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    }
+
+    // 当用户开始进入场景时调用，比如从app switcher(应用切换)选中app
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
+
+    // 当用户停止进入场景，比如进入另一个场景
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+
+    // 当用户将要进入前台，比如从后台开始或者唤醒
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+
+    // 当场景将要进入后台，比如 app 最小化，但是依然在后台运行
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+    }
+
+
+}
+
